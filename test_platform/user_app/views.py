6b0d60038862fdb259f6b1cc40c06f2e457536ab
cici <709695726@qq.com>
2018-09-26 23:36:14 +0800

    优化登录样式及登录验证

--- test_platform/user_app/views.py
+++ test_platform/user_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib import auth
 
 
 # Create your views here.
@@ -11,11 +12,18 @@ def index(request):
 
 # 处理登录请求
 def login_action(request):
-    if request.method == "GET":
-        username = request.GET.get("username")
-        password = request.GET.get("password")
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
 
         if username == "" or password == "":
             return render(request, 'index.html',
-                          {"error": "用户名或密码错误"}
+                          {"error": "用户名或密码为空"}
                           )
+        else:
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)  # 验证登录
+                return render(request, "project_manage.html")
+            else:
+                return render(request, "index.html", {"error": "用户名密码错误"})
