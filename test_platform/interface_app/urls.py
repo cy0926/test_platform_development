e156f2fcdbafb25eb455ef187ee741a194641488
曹颖 <ying.cao@socialcredits.cn>
2018-11-06 18:25:48 +0800

    新增”在线postman“页面，完成基本调试功能

--- test_platform/interface_app/urls.py
+++ test_platform/interface_app/urls.py
@@ -5,5 +5,7 @@ from interface_app import views
 urlpatterns = [
     # 用例管理
     path('case_manage/', views.case_manage),
+    path('debug/', views.debug),
+    path('api_debug/', views.api_debug),
 
 ]
